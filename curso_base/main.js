eb682a65dade8ded02fdd8fb0fe1f049f0372482
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-14 19:37:57 -0300

    aplicar textura al planeta

--- curso_base/main.js
+++ curso_base/main.js
@@ -1,12 +1,15 @@
-// Importar Three.js desde npm
 import * as THREE from 'three';
+// import vertexShader from './shaders/vertex.glsl';
 
 // Inicializar Three.js
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 // Configurar la posición de la cámara
-camera.position.z = 10;
-const renderer = new THREE.WebGLRenderer();
+camera.position.z = 15;
+const renderer = new THREE.WebGLRenderer({
+  antialias: true
+});
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setClearColor(0x000000); // Negro
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('canvas-container').appendChild(renderer.domElement);
@@ -15,7 +18,8 @@ document.getElementById('canvas-container').appendChild(renderer.domElement);
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
   new THREE.MeshBasicMaterial({
-    color: 0xFF0000
+    // color: 0xFF0000,
+    map: new THREE.TextureLoader().load('./img/globe.jpg')
   })
 );
 
