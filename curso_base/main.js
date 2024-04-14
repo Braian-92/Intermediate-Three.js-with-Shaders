b961e250aae5fb73049de1fb91dfc51e9c02b1c9
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-14 19:21:44 -0300

    esfera básica planeta

--- curso_base/main.js
+++ curso_base/main.js
@@ -4,24 +4,26 @@ import * as THREE from 'three';
 // Inicializar Three.js
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// Configurar la posición de la cámara
+camera.position.z = 10;
 const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(0x000000); // Negro
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('canvas-container').appendChild(renderer.domElement);
 
-// Crear un cubo
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
 
-// Configurar la posición de la cámara
-camera.position.z = 5;
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.MeshBasicMaterial({
+    color: 0xFF0000
+  })
+);
+
+scene.add(sphere);
 
 // Función de renderizado
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 animate();
