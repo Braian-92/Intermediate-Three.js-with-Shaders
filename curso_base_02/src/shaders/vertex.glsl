04376a2076eb6fe83e1fb73038695efa4c2d2c32
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-14 22:00:12 -0300

    aplicar glow por fragment

--- curso_base_02/src/shaders/vertex.glsl
+++ curso_base_02/src/shaders/vertex.glsl
@@ -4,9 +4,13 @@ uniform mat4 viewMatrix;
 attribute vec3 position;
 
 varying vec2 vertexUV;
+varying vec3 vertexNormal; // Agregamos vertexNormal
+
 attribute vec2 uv;
+attribute vec3 normal; // Cambiamos de vec2 a vec3 para normal
 
 void main() {
    vertexUV = uv;
+   vertexNormal = normal; // Pasamos el valor de normal a vertexNormal
    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
