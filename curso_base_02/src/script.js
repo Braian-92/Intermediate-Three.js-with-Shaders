be174743778a8bf97b3be2f083cef3dfb663ac67
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-26 22:13:25 -0300

    agregar puntos en latitud y longitud en mundo esferico

--- curso_base_02/src/script.js
+++ curso_base_02/src/script.js
@@ -10,28 +10,30 @@ import at_vShader from "./shaders/atmosphere_vertex.glsl";
 import at_fShader from "./shaders/atmosphere_fragment.glsl";
 
 // npm run dev
+const canvas = document.querySelector("#canvasContainer");
 
 const scene = new THREE.Scene();
 
 //Resizing
 window.addEventListener("resize", () => {
   //Update Size
-  aspect.width = window.innerWidth;
-  aspect.height = window.innerHeight;
+  aspect.width = canvas.offsetWidth;
+  aspect.height = canvas.offsetHeight;
 
   //New Aspect Ratio
   camera.aspect = aspect.width / aspect.height;
   camera.updateProjectionMatrix();
 
   //New RendererSize
-  renderer.setSize(aspect.width, aspect.height);
+  // renderer.setSize(aspect.width, aspect.height);
+  renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
 //Camera
 const aspect = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+  width: canvas.offsetWidth,
+  height: canvas.offsetHeight,
 };
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -42,15 +44,17 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 20;
 scene.add(camera);
 
+
+
 //Renderer
-const canvas = document.querySelector(".draw");
+
 const renderer = new THREE.WebGLRenderer({
   canvas,
   antialias: true,
 });
 renderer.setPixelRatio(window.devicePixelRatio);
 // renderer.setClearColor("2728#2c", 1.0);
-renderer.setSize(aspect.width, aspect.height);
+renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
 
 //OrbitControl
 const orbitControls = new OrbitControls(camera, canvas);
@@ -127,6 +131,42 @@ const clock = new THREE.Clock();
 
 const sphereRotationSpeed = 0.3;
 
+
+const point = new THREE.Mesh(
+  new THREE.SphereGeometry(0.05, 50, 50),
+  new THREE.MeshBasicMaterial({
+    color: '#ff0000'
+  })
+);
+// -34.61315, -58.37723 buenos aires
+// -23.6345 102.5528 mexico
+// arg
+const latitude = -34.61315
+const longitude = -58.37723
+// mex
+// const latitude = 23.6345
+// const longitude = -102.5528
+
+const latitudeRad = (latitude / 180) * Math.PI
+const longitudeRad = (longitude / 180) * Math.PI
+
+const radius = 5
+
+const x = radius * Math.cos(latitudeRad) * Math.sin(longitudeRad)
+const y = radius * Math.sin(latitudeRad)
+const z = radius * Math.cos(latitudeRad) * Math.cos(longitudeRad)
+
+console.log({x, y, z});
+
+point.position.x = x;
+point.position.y = y;
+point.position.z = z;
+
+sphere.rotation.y = -Math.PI / 2
+
+
+group.add(point);
+
 const mouse = {
   x: undefined,
   y: undefined
@@ -144,11 +184,11 @@ const animate = () => {
   //Update Controls
   orbitControls.update();
 
-  sphere.rotation.y = elapsedTime * sphereRotationSpeed;
+  // sphere.rotation.y = elapsedTime * sphereRotationSpeed;
   // group.rotation.y = mouse.x * 0.5;
   gsap.to(group.rotation, {
-    x: -mouse.y * 0.5,
-    y: mouse.x * 0.5,
+    x: -mouse.y * 2.5,
+    y: mouse.x * 2.5,
     duration: 2
   })
 
